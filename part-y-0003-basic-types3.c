c5049900934cdeb61e37ed75248ecb41bb8d2cfe
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-07-10 13:34:15 +0300

    Create part-y-0003-basic-types3.c

---
+++ part-y-0003-basic-types3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+    char first_letter = 'a';
+
+    char last_letter = 'z';
+
+    printf("First letter is: %c and last letter: %c", first_letter, last_letter); // First letter is: a and last letter: z
+
+    return 0;
+}
