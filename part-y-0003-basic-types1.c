8d4349b34333711bf969af4a769a7d391df522d2
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-07-10 13:25:30 +0300

    Create part-y-0003-basic-types1.c

---
+++ part-y-0003-basic-types1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#include <stdint.h>
+
+int main() {
+    
+    int year = 2026;
+
+    int diff = -300;
+
+    uint8_t age;
+
+    age = 17;
+
+    uint16_t salary = 4000;
+
+    return 0;
+}
+
+// gcc main.c -o program ; .\program.exe
