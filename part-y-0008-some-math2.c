559f012723689acab67a114af0a4032c7c62b7dc
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-07-12 23:01:18 +0300

    Create part-y-0008-some-math2.c

---
+++ part-y-0008-some-math2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+    double x = 100;
+
+    double y = sqrt(x);
+
+    printf("y: %.0lf\n", y);
+
+    double l = log10(x);
+
+    printf("l: %.0lf", l);
+}
+
+// y: 10
+// l: 2
