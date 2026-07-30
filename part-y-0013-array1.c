f54203df340056bd9fb79d8281e624bf63a60d2f
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-07-30 21:39:55 +0300

    Create part-y-0013-array1.c

---
+++ part-y-0013-array1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    int years[] = {2000, 1987, 1965};
+
+    printf("He was born in %d.", years[2]); // He was born in 1965.
+
+    return 0;
+}
+
+
+
+// gcc main.c -o program ; .\program.exe
